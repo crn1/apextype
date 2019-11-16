7cb6cd28ceacc06f592c443370c2a3072c0d8798
crn1 <gluvajic@aol.com>
2019-11-16 19:39:44 +0100

    Code refactoring

--- components/MainHeading.js
+++ components/MainHeading.js
@@ -8,37 +8,45 @@ import { useTheme } from '@material-ui/core/styles';
 
 import Typed from 'react-typed';
 
-const Container = styled(Box)({
-	margin: 0,
-	position: 'fixed',
-	top: '30%',
-	left: '50%',
-	transform: 'translate(-50%, -30%)',
-	width: '100%',
-});
-
 const RoseLogo = styled(Box)(({ theme }) => ({
 	marginRight: theme.spacing(2),	
 	display: 'inline',
 }));
 
-const TitleDescription = () => {
+const Quote = () => {
 	const theme = useTheme();
 
 	return (
-		<motion.div
-			style={{opacity: 0}}
-		>
-			<Typography style={{marginTop: theme.spacing(8)}} variant='body1'>
-				<Typed
-					strings={['“The happiest moment I felt; is that moment when I realized my ability to create.”']}
-					typeSpeed={10}
-					showCursor={false}
-				/>
-			</Typography>
-		</motion.div>
+		<Typography style={{marginTop: theme.spacing(8)}} variant='body1'>
+			<Typed
+				strings={['“The happiest moment I felt; is that moment when I realized my ability to <b>create</b>.”']}
+				typeSpeed={3}
+				showCursor={false}
+				startDelay={2500}
+			/>
+		</Typography>
 	);
 } 
+
+const ScrollDown = () => {
+	const theme = useTheme();
+
+	return (
+		<Typography style={{marginTop: theme.spacing(32)}} variant='body1'>
+			<Typed
+				strings={['SCROLL DOWN &#8897;']}
+				typeSpeed={10}
+				showCursor={false}
+				startDelay={3300}
+			/>
+		</Typography>
+	);
+} 
+
+const TitleTypography = styled(Typography)(({ theme }) => ({
+	padding: theme.spacing(2),
+	paddingTop: theme.spacing(24),
+}));
 
 const MainHeading = () => {
 
@@ -46,23 +54,23 @@ const MainHeading = () => {
 	const matches = useMediaQuery(theme.breakpoints.up('sm'));
 
 	return (
-		<Container>
-			<Typography
-					align='center' variant={matches ? 'h1' : 'h3'}>
-				<motion.div
-					initial={{ opacity: 0, y: 150, }}
-					animate={{ opacity: 1, y: 0, }}
-					transition={{ duration: 0.3 }}
-				>
-					<RoseLogo>&#127801;</RoseLogo> 
-					<Typed
-						strings={['apextype']}
-						typeSpeed={70}
-					/>
-					<TitleDescription />
-				</motion.div>
-			</Typography>
-		</Container>
+		<TitleTypography
+				align='center' variant={matches ? 'h1' : 'h3'}>
+			<motion.div
+				initial={{ opacity: 0, y: 300, scale: 0.2, }}
+				animate={{ opacity: 1, y: 0, scale: 1, }}
+				transition={{ duration: 0.3 }}
+			>
+				<RoseLogo>&#127801;</RoseLogo> 
+				<Typed
+					strings={['apextype']}
+					typeSpeed={60}
+					startDelay={1250}
+				/>
+				<Quote />
+				<ScrollDown />
+			</motion.div>
+		</TitleTypography>
 	);
 
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,36 @@
-import { useViewportScroll, useTransform,  motion } from 'framer-motion'
+import { useViewportScroll, useTransform, useSpring, motion } from 'framer-motion'
 import { styled } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
 import blue from '@material-ui/core/colors/blue'
 import purple from '@material-ui/core/colors/purple'
+import { useEffect } from 'react';
 
 import MainHeading from '../components/MainHeading'
 import Layout from '../components/Layout'
 
 const Container = styled(Box)({
 	height: '200vh',
-	width: '100wh',
-	background: `linear-gradient(180deg, ${purple[600]}, ${blue[300]})`,
+	background: `linear-gradient(70deg, ${purple[500]}, ${blue[500]})`,
+})
+
+const Slide = styled(motion.div)({
+	position: 'fixed',
+	width: '100vw',
 })
 
 export default () => {
 
 	const { scrollYProgress } = useViewportScroll();
-	const scale = useTransform(scrollYProgress, [0,1], [0.2, 2]);
+	const yRange = useTransform(scrollYProgress, [0, 1], [1, 2]);
 
 	return (
 		<Layout>
 			<Container>
-				<MainHeading />
+				<Slide
+					style={{scale: yRange}}
+				>
+					<MainHeading />
+				</Slide>
 			</Container>
 		</Layout>
 	);
