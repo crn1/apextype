abd5e8ccd6a54ff29c8ff6c335309ade1e2c5540
crn1 <gluvajic@aol.com>
2019-11-28 22:26:02 +0100

    Reduces meta description

--- components/Layout.js
+++ components/Layout.js
@@ -18,7 +18,7 @@ const Layout = props => (
 			<link rel='shortcut icon' type='image/ico' href='/public/favicon.ico' />
 
 			<meta name='viewport' content='minimum-scale=1, width=device-width, initial-scale=1, shrink-to-fit=no' />
-			<meta name='description' content='My name is Đorđe and I am a full-stack developer from Novi Sad, Serbia. I take a special approach to the development that involves minimalism with emphasis on stability, peformance and scability.' />
+			<meta name='description' content='My name is Đorđe and I am a full-stack developer from Novi Sad, Serbia. I take a special minimalistic approach to the development.' />
 			<meta name='keywords' content='fullstack developer novi sad, web developer serbia, next.js, flask, react.js, web design, landing page, page animations, framer motion developer' />
 			<meta name='author' content='Dorde G.' />
 			<meta charSet='utf-8' />
